4d599a3c0f0fe8a9ad0a0d71faa285422c3e5552
Unknown <aritz.martinezh@gmail.com>
2019-01-19 18:29:28 +0100

    Guardando datos en  Firebase

--- ProyectoFinCurso/app/src/main/java/com/example/msimm/fitegibide/MainActivity.java
+++ ProyectoFinCurso/app/src/main/java/com/example/msimm/fitegibide/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.graphics.Color;
+import android.net.Uri;
 import android.renderscript.Element;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -13,8 +14,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
@@ -39,9 +42,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import io.opencensus.common.Scope;
@@ -49,6 +56,7 @@ import io.opencensus.common.Scope;
 public class MainActivity extends AppCompatActivity implements OnDataPointListener,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
 
     public static final String LOG_TAG = "BasicHistoryApi";
+    public static final String LOG_TAG_FB = "Firebase";
     int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 0533;
     private final String SESSION_NAME = "LogRunActivity";
     SessionInsertRequest insertRequest;
@@ -58,7 +66,12 @@ public class MainActivity extends AppCompatActivity implements OnDataPointListen
     private static final String AUTH_PENDING = "auth_state_pending";
     private boolean authInProgress = false;
     private GoogleApiClient mApiClient;
+    private static final int RC_SIGN_IN = 9001;
+    private String documentID = "";
     TextView cifraPasos;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Map<String, Object> pasos = new HashMap<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +92,7 @@ public class MainActivity extends AppCompatActivity implements OnDataPointListen
 
     @Override
     public void onConnected(Bundle bundle) {
+
         DataSourcesRequest dataSourceRequest = new DataSourcesRequest.Builder()
                 .setDataTypes( DataType.TYPE_STEP_COUNT_CUMULATIVE )
                 .setDataSourceTypes( DataSource.TYPE_RAW )
@@ -147,6 +161,47 @@ public class MainActivity extends AppCompatActivity implements OnDataPointListen
                     Toast.makeText(getApplicationContext(), "Field: " + field.getName() + " Value: " + value, Toast.LENGTH_SHORT).show();
                     cifraPasos =  findViewById(R.id.cifra_pasos);
                     cifraPasos.setText(""+value);
+
+                    //Firebase
+                    Map<String, Object> pasos = new HashMap<>();
+                    pasos.put("pasos", ""+value);
+
+                    //DocumentReference dr = db.collection("pasos").document(documentID);
+                    DocumentReference dr = null;
+                    if (dr == null){
+                        db.collection("pasos")
+                                .add(pasos)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Log.d(LOG_TAG_FB, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(LOG_TAG_FB, "Error adding document", e);
+                                    }
+                                });
+
+                    }else {
+
+                        db.collection("pasos").document(documentID)
+                                .set(pasos)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(LOG_TAG_FB, "DocumentSnapshot Updated " );
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(LOG_TAG_FB, "Error Updating document", e);
+                                    }
+                                });
+                    }
+
                 }
             });
         }
@@ -156,7 +211,9 @@ public class MainActivity extends AppCompatActivity implements OnDataPointListen
     protected void onStart() {
         super.onStart();
         mApiClient.connect();
+
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -173,6 +230,7 @@ public class MainActivity extends AppCompatActivity implements OnDataPointListen
             Log.e("GoogleFit", "requestCode NOT request_oauth");
         }
     }
+
 
     @Override
     protected void onStop() {
@@ -196,118 +254,4 @@ public class MainActivity extends AppCompatActivity implements OnDataPointListen
     }
 
 
-
-
-
-
-
-
-
-/*
-    public void buildFitnessClient(){
-
-        //Instancia de fitnessOptions
-        FitnessOptions fitnessOptions = FitnessOptions.builder()
-                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .build();
-
-
-        //Comprueba si ha aceptado los permisos
-        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
-            GoogleSignIn.requestPermissions(
-                    this, // your activity
-                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
-                    GoogleSignIn.getLastSignedInAccount(this),
-                    fitnessOptions);
-        } else {
-            accessGoogleFit();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-                accessGoogleFit();
-            }
-        }
-    }
-
-    private void accessGoogleFit() {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        long endTime = cal.getTimeInMillis();
-        cal.add(Calendar.YEAR, -1);
-        long startTime = cal.getTimeInMillis();
-
-
-        DataReadRequest readRequest = new DataReadRequest.Builder()
-                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
-                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
-                .build();
-
-
-
-        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
-                .readData(readRequest)
-                .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
-                    @Override
-                    public void onSuccess(DataReadResponse dataReadResponse) {
-                        Log.d(LOG_TAG, "onSuccess()");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e(LOG_TAG, "onFailure()", e);
-                    }
-                })
-                .addOnCompleteListener(new OnCompleteListener<DataReadResponse>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DataReadResponse> task) {
-                        Log.d(LOG_TAG, "onComplete()");
-                    }
-                });
-    }
-    private void addUserDataAndroidHealth() {
-        mSession = new Session.Builder()
-                .setName(SESSION_NAME)
-                .setIdentifier(getString(R.string.app_name) + " " + System.currentTimeMillis())
-                .setDescription("Running Session Details")
-                .setStartTime((long) startTime, TimeUnit.MILLISECONDS)
-                .setEndTime((long) endTime, TimeUnit.MILLISECONDS)
-                .setActivity(FitnessActivities.RUNNING)
-                .build();
-        DataSource distanceSegmentDataSource = new DataSource.Builder()
-                .setAppPackageName(this.getPackageName())
-                .setDataType(DataType.AGGREGATE_DISTANCE_DELTA)
-                .setName("Total Distance Covered")
-                .setType(DataSource.TYPE_RAW)
-                .build();
-        DataSet distanceDataSet = DataSet.create(distanceSegmentDataSource);
-        DataPoint firstRunSpeedDataPoint = distanceDataSet.createDataPoint()
-                .setTimeInterval((long) startTime, (long) endTime, TimeUnit.MILLISECONDS);
-        firstRunSpeedDataPoint.getValue(Field.FIELD_DISTANCE).setFloat((float) totalCoveredDistanceInMiles);
-        distanceDataSet.add(firstRunSpeedDataPoint);
-        DataSource caloriesDataSource = new DataSource.Builder()
-                .setAppPackageName(this.getPackageName())
-                .setDataType(DataType.AGGREGATE_CALORIES_EXPENDED)
-                .setName("Total Calories Burned")
-                .setType(DataSource.TYPE_RAW)
-                .build();
-        DataSet caloriesDataSet = DataSet.create(caloriesDataSource);
-        DataPoint caloriesDataPoint = caloriesDataSet.createDataPoint()
-                .setTimeInterval((long) startTime, (long) endTime, TimeUnit.MILLISECONDS);
-        caloriesDataPoint.getValue(Field.FIELD_CALORIES).setFloat((float) totalCalories);
-        caloriesDataSet.add(caloriesDataPoint);
-        insertRequest = new SessionInsertRequest.Builder()
-                .setSession(mSession)
-                .addDataSet(distanceDataSet)
-                .addDataSet(caloriesDataSet)
-                .build();
-        new InsertIUserHistoryDataGoogleFit().execute("", "", "");
-    }
-
-*/
 }
